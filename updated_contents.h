ec949529951dde0775631aee520c3a4c3a5d802f
drewkim20 <102550676+drewkim20@users.noreply.github.com>
2023-03-16 00:12:24 -0700

    Create updated_contents.h

---
+++ updated_contents.h
@@ -0,0 +1,20 @@
+#ifndef CONTENTS
+#define CONTENTS
+
+#include "util.h"
+
+void list_files(int fd, int vflag, char *given, int specific, int extract);
+void write_permissions(char mode[], int fd, unsigned offset);
+char get_type(int fd, unsigned offset);
+void write_owner_group(int fd, unsigned offset);
+long int get_size(int fd, unsigned offset);
+void write_mtime(int fd, unsigned offset);
+char *write_uid(int fd, unsigned offset);
+char *write_gid(int fd, unsigned offset);
+char *get_prefix(int fd, unsigned offset);
+char *get_mtime(int fd, unsigned offset);
+char *get_xname(char *name);
+
+
+
+#endif
